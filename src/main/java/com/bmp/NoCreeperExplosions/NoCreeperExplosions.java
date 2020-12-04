9551eb251d5e06de18a7f6184c43329740026676
Ben Pringle <benjamin.m.pringle@gmail.com>
2020-12-03 23:52:15 -0500

    Initial Commit

---
+++ src/main/java/com/bmp/NoCreeperExplosions/NoCreeperExplosions.java
@@ -0,0 +1,25 @@
+package com.bmp.NoCreeperExplosions;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftCreeper;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoCreeperExplosions  extends JavaPlugin implements Listener {
+	@Override
+    public void onEnable() {
+    	getLogger().info("onEnable has been invoked!");
+    	this.getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    @Override
+    public void onDisable() {
+    	getLogger().info("onDisable has been invoked!");
+    }
+    
+    @EventHandler
+    public void entityExplodeEvent(org.bukkit.event.entity.EntityExplodeEvent event){
+    	if(event.getEntity() instanceof CraftCreeper) {
+    		event.blockList().clear();
+    	}
+    }
+}
